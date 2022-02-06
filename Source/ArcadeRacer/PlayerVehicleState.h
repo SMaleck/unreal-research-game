89a9fc07d988f15e39ef0c009a632eab12f305f7
Sebastian Maleck <s.maleck@outlook.de>
2022-02-06 22:49:08 +0100

    Adds Empty Code

---
+++ Source/ArcadeRacer/PlayerVehicleState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PlayerVehicleState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCADERACER_API UPlayerVehicleState : public UObject
+{
+	GENERATED_BODY()
+	
+};
